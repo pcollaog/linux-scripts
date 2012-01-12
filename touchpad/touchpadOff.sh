3b956fd68ac2204d9e9c67db1de87ebce17a38d4
Francisco Collao Garate <francisco.collao@excelsys.net>
2012-01-12 11:57:38 -0300

    Se agregan script para touchpad

---
+++ touchpad/touchpadOff.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 8 0
+#synclient TouchpadOff=1
---
+++ touchpad/touchpadOn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xinput set-int-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 8 1
+#synclient TouchpadOff=1
